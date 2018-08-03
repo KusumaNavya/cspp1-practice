7fb73b55d4ab0c2f3f0d0d450307329f88a587eb
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-03 18:16:24 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

---
+++ M5/p1/p1/perfect_cube.py
@@ -0,0 +1,13 @@
+"""perfect cube"""
+def main():
+    """code is here"""
+    guess = 0
+    x_val = int(input())
+    while guess**3 < x_val:
+        guess = guess + 1
+    if guess**3 == x_val:
+        print(x_val, "is a perfect cube")
+    else:
+        print(x_val, "is not a perfect cube")
+if __name__ == "__main__":
+    main()
